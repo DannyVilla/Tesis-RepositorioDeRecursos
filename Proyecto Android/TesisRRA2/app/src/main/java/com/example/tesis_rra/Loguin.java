1257a9c295a063203cca5913ea77dfc1482bbeba
DannyVilla <43586615+DannyVilla@users.noreply.github.com>
2020-03-30 21:35:51 -0600

    Front-End Loguin
    Implementación de front-end y conexión con firebase

---
+++ Proyecto Android/TesisRRA2/app/src/main/java/com/example/tesis_rra/Loguin.java
@@ -0,0 +1,23 @@
+package com.example.tesis_rra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Loguin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_loguin);
+    }
+    public void Siguiente(View view){
+        Intent siguiente = new Intent(this, Home.class);
+        startActivity(siguiente);
+
+
+    }
+
+}
